d0420e0f4dd640338aa8198cbdbf6e64299ceec1
RobertoPatino1 <rapatino@espol.edu.ec>
2023-11-05 18:36:42 -0500

    Added function to test tokens

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import ply.lex as lex
 #--------------------------
 # Aporte Roberto Patiño
 #--------------------------
-# Palabras reservadas
+# ---Palabras reservadas---
 reservadas = {'if':'IF', 'while': 'WHILE', 'else': 'ELSE', 'case' : 'CASE', 
               'break': 'BREAK', 'continue': 'CONTINUE', 'do': 'DO', 'echo': 'ECHO', 
               'elseif': 'ELSEIF', 'endwhile': 'ENDWHILE', 'for': 'FOR', 'foreach': 
@@ -12,6 +12,7 @@ reservadas = {'if':'IF', 'while': 'WHILE', 'else': 'ELSE', 'case' : 'CASE',
 # Secuencia de tokens
 tokens = (
     'ID',
+    'EOL',
     'ENTERO',
     'FLOTANTE',
     'BOOLEANO',
@@ -50,3 +51,100 @@ tokens = (
     'COMILLA_SIMPLE_CADENA',
 )+tuple(reservadas.values())
 
+# ---Expresiones regulares simples para simbolos---
+# Simbolos aritmeticos
+t_SUMA = r'\+'
+t_RESTA = r'\-'
+t_MULT = r'\*'
+t_DIV = r'\/'
+t_POT = r'\*\*'
+
+# Simbolos de comparacion
+t_IGUAL = r'\=='
+t_DIFERENTE = r'\!='
+t_MENOR = r'\<'
+t_MAYOR = r'\>'
+t_MENOR_IGUAL = r'\>='
+t_MAYOR_IGUAL = r'\>='
+
+# Simbolos para delimitar bloques
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_EOL = r'\;'
+
+
+# ---Identificadores---
+
+
+# ---Numeros---
+
+
+# ---Strings---
+
+
+
+#---Definicion de funciones---
+
+# Identificadores
+def t_ID(t):
+  r'^\$([a-zA-z]|_)+\w*[^\$\s]*$'
+  t.type = reservadas.get(t.value, 'ID')
+  return t
+
+# ---Tipos de datos basicos---
+
+# Flotantes
+def t_FLOTANTE(t):
+  r'\d+\.\d+'
+  t.value = float(t.value)
+  return t
+
+# Enteros
+def t_ENTERO(t):
+  r'\d+'
+  t.value = int(t.value)
+  return t
+
+# Cadenas (TODO: SOLO FUNCIONA PARA COMILLAS DOBLES)
+def t_CADENA(t):
+  r'"([^"\n]|(\\"))*"$'
+  t.value = str(t.value)
+  return t
+
+# Booleanos
+def t_BOOLEANO(t):
+  r'^(true|false)$'
+  t.value = int(t.value)
+  return t
+
+
+# Manejo de errores
+def t_error(t):
+  print(
+      f"{t.type.upper()}: No se reconoce el caracter {t.value[0]} en la línea {t.lineno}"
+  )
+  t.lexer.skip(1)
+
+# Expresión regular para reconocer saltos de línea
+def t_newline(t):
+  r'\n+'
+  t.lexer.lineno += len(t.value)
+
+
+# Ignorar espacios, tabulaciones
+t_ignore = ' \t'
+
+
+# Construye el lexer
+lexer = lex.lex()
+
+#################################################################
+# Funcion para validar que los tokens sean validos (Reutilizable)
+#################################################################
+def test_tokens(code):
+  lexer.input(code)
+  for token in lexer:
+    print(token)
+test_tokens('''
+            12.5**2
+            ''')
