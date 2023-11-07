8b54881e62f87e538fd360a37b6f77a14b309299
RobertoPatino1 <rapatino@espol.edu.ec>
2023-11-06 22:47:49 -0500

    Finished project 1

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ import ply.lex as lex
 # Aporte Roberto Patiño
 #--------------------------
 # ---Palabras reservadas---
-reservadas = {'if':'IF', 'while': 'WHILE', 'else': 'ELSE', 'case' : 'CASE', 
-              'break': 'BREAK', 'continue': 'CONTINUE', 'do': 'DO', 'echo': 'ECHO', 
-              'elseif': 'ELSEIF', 'endwhile': 'ENDWHILE', 'for': 'FOR', 'foreach': 
-              'FOREACH', 'function': 'FUNCTION', 'global': 'GLOBAL', 
-              'return': 'RETURN', 'switch': 'SWITCH'}
+reservadas = {
+    'if': 'IF',
+    'while': 'WHILE',
+    'else': 'ELSE',
+    'case': 'CASE',
+    'break': 'BREAK',
+    'continue': 'CONTINUE',
+    'do': 'DO',
+    'echo': 'ECHO',
+    'elseif': 'ELSEIF',
+    'endwhile': 'ENDWHILE',
+    'for': 'FOR',
+    'foreach': 'FOREACH',
+    'function': 'FUNCTION',
+    'global': 'GLOBAL',
+    'return': 'RETURN',
+    'switch': 'SWITCH'
+}
 # Secuencia de tokens
 tokens = (
+    # Roberto ------------------
     'ID',
     'ASIGNACION',
     'VAR',
@@ -20,11 +34,6 @@ tokens = (
     'FLOTANTE',
     'BOOLEANO',
     'CADENA',
-    'SUMA',
-    'RESTA',
-    'MULT',
-    'DIV',
-    'POT',
     'LPAREN',
     'RPAREN',
     'LBRACKET',
@@ -52,27 +61,30 @@ tokens = (
     'COMILLA_SIMPLE',
     'COMILLA_DOBLE_CADENA',
     'COMILLA_SIMPLE_CADENA',
-    'VARIABLE_ARRAY',
-    'VARIABLE_FUNCION',
-    'VARIABLE_OBJETO',
-    'ARRAY',
-    'FUNCION', 
-    'OBJETO',
-    'LIST',
+    # Daniel -------------------
+    # Operadores
+    'SUMA',
+    'RESTA',
+    'MULT',
+    'DIV',
+    'POT',
+    # Tipos de datos avanzados
     'DICT',
     'TUPLE',
     'SET',
+    # Cristopher ---------------
     'VARIABLE_ARRAY',
     'VARIABLE_FUNCION',
     'VARIABLE_OBJETO',
     'ARRAY',
-    'FUNCION', 
-    'OBJETO'
-)+tuple(reservadas.values())
+    'FUNCION',
+    'OBJETO') + tuple(reservadas.values())
 
 # ---Expresiones regulares simples para simbolos o caracteres especiales---
+
 # Asignacion
 t_ASIGNACION = r'\='
+t_COMA = r'\,'
 # Simbolos aritmeticos
 t_SUMA = r'\+'
 t_RESTA = r'\-'
@@ -97,6 +109,36 @@ t_EOL = r'\;'
 
 
 #---Definicion de funciones---
+# Aporte Cristopher -------
+# Variable Array
+def t_VARIABLE_ARRAY(t):
+  r'array\((((\d+)(,\s\d)+)|("\w+"(,\s"\w+")*)|(\$\w+(,\s\$\w+)*))\)'
+  return t
+
+
+# Variable Funcion
+def t_VARIABLE_FUNCION(t):
+  r'[a-z]\w*\(\$\w+(,\s\$\w+)*\)'
+  return t
+
+
+# Variable Objeto
+def t_VARIABLE_OBJETO(t):
+  r'\$[a-z]\w*->[a-z]\w*'
+  return t
+
+
+# Funcion
+def t_FUNCION(t):
+  r'[a-z]\w*\(\)'
+  return t
+
+
+# Objeto
+def t_OBJETO(t):
+  r'[A-Z][a-z]*'
+  return t
+
 
 # Comentarios
 def t_COMNT(t):
@@ -105,7 +147,22 @@ def t_COMNT(t):
   return t
 
 
+# Identificadores
+def t_ID(t):
+  r'[a-z_]\w*'
+  t.type = reservadas.get(t.value, 'ID')
+  return t
+
+
+# Definicion de variables
+def t_VAR(t):
+  r'\$([a-zA-z]|_)+\w*[^\$\s]*'
+  t.type = reservadas.get(t.value, 'VAR')
+  return t
+
+
 # ---Tipos de datos basicos---
+
 
 # Flotantes
 def t_FLOTANTE(t):
@@ -113,97 +170,52 @@ def t_FLOTANTE(t):
   t.value = float(t.value)
   return t
 
+
 # Enteros
 def t_ENTERO(t):
   r'\d+'
   t.value = int(t.value)
   return t
 
-# Cadenas 
+
+# Cadenas
 def t_CADENA(t):
   r'"([^"\n]|(\\"))*"|\'([^\'\n]|(\\\'))*\''
   t.value = str(t.value)
   return t
 
+
 # Booleanos
 def t_BOOLEANO(t):
-  r'True|False'
-  if t.value =='True':
-    t.value = True
-  else:
-    t.value = False
+  r'true|false'
+  t.value = bool(t.value)
   return t
+
 
 # Aporte Daniel -------
 
-  # ---- Tipos de datos avanzados ----
-  
+# ---- Tipos de datos avanzados ----
+
+
 # Listas
-def t_LIST(t):
-  r'\[[^\]]*\]'
+def t_ARRAY(t):
+  r'\[(((\d+)(,\s?\d)+)|("\w+"(,\s?"\w+")*)|(\$\w+(,\s?\$\w+)*))\]'
   return t
-  
+
+
 # Diccionarios
 def t_DICT(t):
   r'\=\s?{[^\}]*\}'
   return t
-  
-# Tuplas
-def t_TUPLE(t):
-  # r'\=\s?([^\)]*\\)' tuple()
-  r'\(.*?,.*?\)'
-  return t
-  
-# Set 
-def t_SET(t):
-  r'\=\s?{[^\}]*\}'
-  return t
 
-# Aporte Cristopher --------
 
-# Variable Array
-def t_VARIABLE_ARRAY(t):
-  r'^array\((((\d+)(, \d)+)|("\w+"(, "\w+")*)|(\$\w+(, \$\w+)*))\)$'
-  return t
-
-# Variable Funcion
-def t_VARIABLE_FUNCION(t):
-  r'^[a-z]\w*\(\$\w+(, \$\w+)*\)$'
-  return t
-# Variable Objeto
-def t_VARIABLE_OBJETO(t):
-  r'^\$[a-z]\w*->[a-z]\w*$'
-  return t
-# Array
-def t_ARRAY(t):
-  r'^\[(((\d+)(, \d)+)|("\w+"(, "\w+")*)|(\$\w+(, \$\w+)*))\]$'
-  return t
-# Funcion
-def t_FUNCION(t):
-  r'^[a-z]\w*\(\)$'
-  return t
-# Objeto
-def t_OBJETO(t):
-  r'^[A-Z][a-z]*$'
-  return t
-
-# Identificadores
-def t_ID(t):
-    r'[a-z_]\w*'
-    t.type = reservadas.get(t.value, 'ID')
-    return t
-
-# Definicion de variables
-def t_VAR(t):
-  r'\$[a-zA-Z_][a-zA-Z0-9_]*'
-  t.type = reservadas.get(t.value, 'VAR')
-  return t
 # Manejo de errores
 def t_error(t):
   print(
       f"{t.type.upper()}: No se reconoce el caracter {t.value[0]} en la línea {t.lineno}"
   )
   t.lexer.skip(1)
+
 
 # Expresión regular para reconocer saltos de línea
 def t_newline(t):
@@ -214,9 +226,9 @@ def t_newline(t):
 # Ignorar espacios, tabulaciones
 t_ignore = ' \t'
 
-
 # Construye el lexer
 lexer = lex.lex()
+
 
 # #################################################################
 # # Funcion para validar que los tokens sean validos (Reutilizable)
@@ -226,10 +238,11 @@ def test_tokens(code):
   for token in lexer:
     print(token)
 
+
 # Prueba Daniel
 print("\n---- Prueba Daniel ----\n")
 
-codigo_prueba_daniel = "[3,4,5,6] {asd: asd} (4,5,6) ; x = 3 + - * /"
+codigo_prueba_daniel = '''[3,4,5,6] {asd: asd} (4,5,6) ; x = 3 + - * /'''
 test_tokens(codigo_prueba_daniel)
 
 # Prueba Roberto
@@ -271,9 +284,31 @@ test_tokens('''
 
 # Prueba Cristopher
 print("\n---- Prueba Cristopher ----\n")
-
 codigo_prueba_cristopher = '''
+array(1, 2, 3, 4)
+array($persona1, $persona2, $persona3)
+array("rojo", "verde", "azul")
+
+promedio($numeros)
+suma($a, $b, $c)
+promedio_total($numeros)
+suma_total($a, $b, $c)
+
+$this->nombre
+$juan->apellido
+$juan5->apellido
+
+[1, 2, 3, 4, 5]
+[$persona1, $persona2, $persona3]
+["rojo", "verde", "azul"]
+
+promedio()
+promedio_total()
+
+Persona
+Casa
+Carro
+Oficina
 
 '''
-
 test_tokens(codigo_prueba_cristopher)
