f8dd4c5072242bffd49d1c28a67aeb9ba2a5899f
RobertoPatino1 <rapatino@espol.edu.ec>
2023-11-12 11:28:37 -0500

    Added variable declaration syntax

---
+++ main.py
@@ -0,0 +1,46 @@
+import ply.yacc as sint
+from lexico import tokens
+# Roberto: Declaración de Variables, Comentarios, Identificadores, Strings.
+#Primera regla
+
+#--------------------------
+# Aporte Roberto Patiño
+#--------------------------
+# ---Definicion de sentencias---
+def p_sentencia(p):
+  '''sentencia : asignacion
+  '''
+
+# ---Declaracion y asignacion de variables---
+def p_asignacion(p):
+  "asignacion : VAR ASIGNACION valor EOL"
+
+
+
+
+#OJO: BOOLEANO NO FUNCIONA
+# VARIABLE_ARRAY NO FUNCIONA PARA ARRAYS DE 1 SOLO ELEMENTO
+
+def p_valor(p):
+  '''valor : ENTERO
+          | FLOTANTE
+          | BOOLEANO    
+          | CADENA
+          | VAR
+          | VARIABLE_ARRAY
+  '''
+
+def p_error(p):
+  print("Error de sintaxis")
+
+parser = sint.yacc()
+
+while True:
+   try:
+       s = input('calc >>> ')
+   except EOFError:
+       break
+   if not s: continue
+   result = parser.parse(s)
+   if result != None:
+     print(result)
